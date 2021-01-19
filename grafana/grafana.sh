f43f73ba3fc7f51958c0b4a43fdf780eeb3a8766
Andrei Popa <andrei@hashicorp.com>
2021-01-19 15:37:29 +0100

    grafana prometheus mv

---
+++ grafana/grafana.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+which grafana-server &>/dev/null || {
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get install -y gnupg
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8C8C34C524098CB6
+  echo 'deb https://packages.grafana.com/oss/deb stable main' | tee /etc/apt/sources.list.d/grafana.list
+  curl -fsSL https://packages.grafana.com/gpg.key | apt-key add -
+  apt-get update
+  apt-get install -y grafana apt-transport-https
+  systemctl daemon-reload
+  systemctl enable grafana-server
+  systemctl start grafana-server
+}
+
+[ -d /etc/consul.d ] && {
+  curl -sL -o /etc/consul.d/grafana.hcl https://raw.githubusercontent.com/andrewpopa/bash-provisioning/main/grafana/grafana.hcl
+  systemctl reload consul_client.service
+}
